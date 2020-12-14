521af77ee0c6247bebb7530f8f786fafba632c18
Maksim Petelin <mpetelin@accesssoftek.com>
2020-12-14 18:46:35 +0500

    functions were added

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
+const db = require('./database.json')
 
 var schema = buildSchema(`
   type Person {
-      name: String,
-      age: Int,
-      origin: String
+      name: String!,
+      age: Int!,
+      origin: String!,
+      city: String!,
+      car: String
   }
 
   type Query {
     hello: String,
-    persons: [Person]
+    persons: [Person],
+    from1toN(N: Int!): [Int],
+    fromNto1(N: Int!): [Int],
+    getPersonByName(name: String!): [Person],
+    getPersonByCar(marka: String!): [Person]
   }
 `);
- 
+
 var root = {
     hello: () => 'Hello world!',
-    persons: () => {
-        return [
-            { name: 'Max', age: 36, origin: 'Russian' },
-            { name: 'Andrew', age: 33, origin: 'Russian' },
-        ]
-    }
+    persons: () => db.persons.map(getPersonDisplay),
+    from1toN: ({N}) => Array.from(Array(N).keys()),
+    fromNto1: ({N}) => Array.from(Array(N).keys()).reverse(),
+    getPersonByName: ({name}) => db.persons.filter(p => p.name === name).map(getPersonDisplay),
+    getPersonByCar: ({marka}) => db.persons.filter(p => db.cars.filter(c => c.marka === marka).map(c => c.Id).some(id => id === p.carId)).map(getPersonDisplay),
 };
+
+function getPersonDisplay(person) {
+  const car = db.cars.find(c => c.Id === person.carId);
+  return Object.assign({}, person, { car: car ? `${car.marka}, ${car.model}` : '' });
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
